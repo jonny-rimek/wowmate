4397067d9cd737d6c426e28de8068c941c5d70ee
Jonny Rimek <jimbo.db@protonmail.com>
2020-03-25 17:39:50 +0100

    Update toHash formatting

--- services/upload/import/import.go
+++ services/upload/import/import.go
@@ -152,7 +152,6 @@ func parseCSV(file []byte) ([]golib.DamageSummary, error) {
 	scanner := bufio.NewScanner(reader)
 	
 	var toHash strings.Builder
-	var toHash2 string
 
 	scanner.Scan() //skips the first line, which is the header of the csv
 	for scanner.Scan() {
@@ -171,8 +170,7 @@ func parseCSV(file []byte) ([]golib.DamageSummary, error) {
 		casterID := trimQuotes(row[3])
 		casterName := trimQuotes(row[4])
 
-		toHash.WriteString(fmt.Sprintf("%v-%v", casterID, damage))
-		toHash2 += fmt.Sprintf("%v-%v", casterID, damage)
+		toHash.WriteString(fmt.Sprintf("|%v|%v", casterID, damage))
 
 		r := golib.DamageSummary{
 			BossFightUUID: bossFightUUID, 
@@ -185,10 +183,9 @@ func parseCSV(file []byte) ([]golib.DamageSummary, error) {
 		records = append(records, r)
 	}
 	sha1 := sha1.New()
-	hash := fmt.Sprintf("%v-%v", records[0].EncounterID, toHash.String())
+	hash := fmt.Sprintf("%v%v", records[0].EncounterID, toHash.String())
 
 	logrus.Debug("pre hash" + hash)
-	logrus.Debug("hash2" + toHash2)
 	logrus.Debug("hash " + fmt.Sprintf("%x", sha1.Sum([]byte(hash))))
 
 	logrus.Debug("read CSV into structs")
