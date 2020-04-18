319a54553d0dc166fdf16d4b4a9fdf8d3cda1bc4
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-19 01:49:37 +0200

    Add prefetching

--- services/frontend/components/NavbarLink.vue
+++ services/frontend/components/NavbarLink.vue
@@ -1,11 +1,11 @@
 <template>
-  <router-link
+  <nuxt-link
     :to="{ name: link }"
     active-class="border-red-800 text-gray-700 hover:border-red-800"
     class="text-gray-600 hover:text-gray-700 hover:border-gray-500 focus:outline-none transition duration-150 ease-in-out"
   >
     <slot></slot>
-  </router-link>
+  </nuxt-link>
 </template>
 <script>
 export default {
