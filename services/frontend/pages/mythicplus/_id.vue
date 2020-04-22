f4c4fce3705205c3980030f1b330752d9b5d9695
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-22 20:28:28 +0200

    Favicon from eps

--- services/frontend/pages/mythicplus/_id.vue
+++ services/frontend/pages/mythicplus/_id.vue
@@ -17,7 +17,7 @@
                 <th
                   class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  M+ stufe
+                  level
                 </th>
                 <th
                   class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
