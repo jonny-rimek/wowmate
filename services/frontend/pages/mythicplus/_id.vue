3c2d886ba9554e0360df4cc4aea9aef87d682552
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-22 23:39:10 +0200

    Add example to m+ overview

--- services/frontend/pages/mythicplus/_id.vue
+++ services/frontend/pages/mythicplus/_id.vue
@@ -11,27 +11,78 @@
                 <th
                   class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                 >
+                  Dungeon
+                </th>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                >
                   Name
                 </th>
                 <th
                   class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Title
-                </th>
+                ></th>
                 <th
                   class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Status
-                </th>
-                <th
-                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Role
+                  Damage
                 </th>
                 <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
               </tr>
             </thead>
             <tbody class="bg-white">
+              <tr>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm leading-5 font-medium text-gray-900">
+                        Tempel of Sethraliss
+                        <span class="text-gray-500">+23*</span>
+                      </div>
+                      <div class="text-sm leading-5 text-gray-500">
+                        <div>affixes:</div>
+                        <div>34:23 +3:12</div>
+                        <div>4 deaths</div>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="text-sm leading-5 text-gray-900">Terra</div>
+                  <div class="text-sm leading-5 text-gray-900">Xava</div>
+                  <div class="text-sm leading-5 text-gray-900">Micha</div>
+                  <div class="text-sm leading-5 text-gray-900">Holytank</div>
+                  <div class="text-sm leading-5 text-gray-900">Tova</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div>||||||||||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||</div>
+                  <div>||||||||||||||||||</div>
+                  <div>|||||||||||</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="text-sm leading-5 text-gray-900">56.535</div>
+                  <div class="text-sm leading-5 text-gray-900">46.535</div>
+                  <div class="text-sm leading-5 text-gray-900">36.535</div>
+                  <div class="text-sm leading-5 text-gray-900">26.535</div>
+                  <div class="text-sm leading-5 text-gray-900">16.535</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
+                >
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                    >more</a
+                  >
+                </td>
+              </tr>
               <tr>
                 <td
                   class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
@@ -52,11 +103,20 @@
                 <td
                   class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
                 >
+                  <div class="text-sm leading-5 text-gray-900">Terra</div>
+                  <div class="text-sm leading-5 text-gray-900">Xava</div>
+                  <div class="text-sm leading-5 text-gray-900">Micha</div>
+                  <div class="text-sm leading-5 text-gray-900">Holytank</div>
                   <div class="text-sm leading-5 text-gray-900">Tova</div>
-                  <div class="text-sm leading-5 text-gray-900">Tova</div>
-                  <div class="text-sm leading-5 text-gray-900">Tova</div>
-                  <div class="text-sm leading-5 text-gray-900">Tova</div>
-                  <div class="text-sm leading-5 text-gray-900">Tova</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div>||||||||||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||</div>
+                  <div>||||||||||||||||||</div>
+                  <div>|||||||||||</div>
                 </td>
                 <td
                   class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
@@ -68,15 +128,10 @@
                   <div class="text-sm leading-5 text-gray-900">16.535</div>
                 </td>
                 <td
-                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500"
-                >
-                  Owner
-                </td>
-                <td
                   class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
                 >
                   <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                    >Edit</a
+                    >more</a
                   >
                 </td>
               </tr>
--- services/frontend/pages/mythicplus/index.vue
+++ services/frontend/pages/mythicplus/index.vue
@@ -1,5 +1,146 @@
 <template>
-  <div>m+</div>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-5">
+    <div class="flex flex-col">
+      <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div
+          class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200"
+        >
+          <table class="min-w-full">
+            <thead>
+              <tr>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Dungeon
+                </th>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                ></th>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Damage
+                </th>
+                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+              </tr>
+            </thead>
+            <tbody class="bg-white">
+              <tr>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm leading-5 font-medium text-gray-900">
+                        Tempel of Sethraliss
+                        <span class="text-gray-500">+23*</span>
+                      </div>
+                      <div class="text-sm leading-5 text-gray-500">
+                        <div>affixes:</div>
+                        <div>34:23 +3:12</div>
+                        <div>4 deaths</div>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="text-sm leading-5 text-gray-900">Terra</div>
+                  <div class="text-sm leading-5 text-gray-900">Xava</div>
+                  <div class="text-sm leading-5 text-gray-900">Micha</div>
+                  <div class="text-sm leading-5 text-gray-900">Holytank</div>
+                  <div class="text-sm leading-5 text-gray-900">Tova</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div>||||||||||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||</div>
+                  <div>||||||||||||||||||</div>
+                  <div>|||||||||||</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="text-sm leading-5 text-gray-900">56.535</div>
+                  <div class="text-sm leading-5 text-gray-900">46.535</div>
+                  <div class="text-sm leading-5 text-gray-900">36.535</div>
+                  <div class="text-sm leading-5 text-gray-900">26.535</div>
+                  <div class="text-sm leading-5 text-gray-900">16.535</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
+                >
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                    >more</a
+                  >
+                </td>
+              </tr>
+              <tr>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm leading-5 font-medium text-gray-900">
+                        Atal'Dazar <span class="text-gray-500">+23*</span>
+                      </div>
+                      <div class="text-sm leading-5 text-gray-500">
+                        <div>affixes:</div>
+                        <div>34:23 +3:12</div>
+                        <div>4 deaths</div>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="text-sm leading-5 text-gray-900">Terra</div>
+                  <div class="text-sm leading-5 text-gray-900">Xava</div>
+                  <div class="text-sm leading-5 text-gray-900">Micha</div>
+                  <div class="text-sm leading-5 text-gray-900">Holytank</div>
+                  <div class="text-sm leading-5 text-gray-900">Tova</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div>||||||||||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||||||||||||</div>
+                  <div>|||||||||||||||||||||||||||||</div>
+                  <div>||||||||||||||||||</div>
+                  <div>|||||||||||</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+                >
+                  <div class="text-sm leading-5 text-gray-900">56.535</div>
+                  <div class="text-sm leading-5 text-gray-900">46.535</div>
+                  <div class="text-sm leading-5 text-gray-900">36.535</div>
+                  <div class="text-sm leading-5 text-gray-900">26.535</div>
+                  <div class="text-sm leading-5 text-gray-900">16.535</div>
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
+                >
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                    >more</a
+                  >
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
