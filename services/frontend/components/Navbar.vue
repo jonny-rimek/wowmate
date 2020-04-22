14ecb13f20cba77d1736dcda72b9b0055f3148a6
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-22 15:59:05 +0200

    Add logo only picture

--- services/frontend/components/Navbar.vue
+++ services/frontend/components/Navbar.vue
@@ -13,7 +13,7 @@
               >
                 <img
                   class="h-12 focus:outline-none w-auto"
-                  src="/wowmate_logo_full_transparent.png"
+                  src="/wowmate_logo_only_transparent.png"
                   alt=""
                 />
               </nuxt-link>
--- services/frontend/pages/mythicplus/_id.vue
+++ services/frontend/pages/mythicplus/_id.vue
@@ -1,6 +1,45 @@
 <template>
-  <div>
-    blub
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-5">
+    <div class="flex flex-col">
+      <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div
+          class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200"
+        >
+          <table class="min-w-full">
+            <thead>
+              <tr>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Damage
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white">
+                <td
+                  class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900"
+                >
+                  Bernard Lane
+                </td>
+                <td
+                  class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500"
+                >
+                  Director, Human Resources
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <!-- </div> -->
+    <!-- Content goes here -->
   </div>
 </template>
 
