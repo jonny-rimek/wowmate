b4a46ae61dcd5499f641ee6397f9f614b9693fa5
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-28 19:36:21 +0200

    Deactivate not v2 stff

--- lib/wowmate-stack.ts
+++ lib/wowmate-stack.ts
@@ -9,17 +9,17 @@ export class Wowmate extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		const db = new Database(this, 'Database')
+		// const db = new Database(this, 'Database')
 
-		new Api(this, 'Api', {
-			dynamoDB: db.dynamoDB,
-		})
+		// new Api(this, 'Api', {
+		// 	dynamoDB: db.dynamoDB,
+		// })
 
-		new Frontend(this, 'Frontend')
+		// new Frontend(this, 'Frontend')
 		
-		new Upload(this, 'Upload', {
-			dynamoDB: db.dynamoDB,
-		})
+		// new Upload(this, 'Upload', {
+		// 	dynamoDB: db.dynamoDB,
+		// })
 
 		new V2(this, 'V2')
 	}
