44df2919ab4c7078e35f78bb99be32b14ae3c837
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-28 21:03:08 +0200

    Remove random vpc stufd

--- lib/v2.ts
+++ lib/v2.ts
@@ -6,16 +6,7 @@ export class V2 extends cdk.Construct {
 	constructor(scope: cdk.Construct, id: string) {
 		super(scope, id)
 
-		const vpc = new ec2.Vpc(this, 'Vpc', {
-			cidr: '10.0.0.0/16',
-			maxAzs: 2,
-			subnetConfiguration: [{
-				cidrMask: 26,
-				name: 'publicSubnet',
-				subnetType: ec2.SubnetType.PUBLIC,
-			}],
-			natGateways: 0
-		})
+		const vpc = new ec2.Vpc(this, 'Vpc')
 
 		const postgres = new rds.DatabaseInstance(this, 'Postgres', {
 			engine: rds.DatabaseInstanceEngine.POSTGRES,
