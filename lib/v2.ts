c897669574e11df7f574bb1b1ba79662f54feb95
Jonny Rimek <jimbo.db@protonmail.com>
2020-04-28 19:53:47 +0200

    Remove all

--- lib/v2.ts
+++ lib/v2.ts
@@ -6,19 +6,5 @@ export class V2 extends cdk.Construct {
 	constructor(scope: cdk.Construct, id: string) {
 		super(scope, id)
 
-		const vpc = new ec2.Vpc(this, 'Vpc', {
-			// subnetConfiguration: [{
-			// 	subnetType: ec2.SubnetType.PUBLIC,
-			// 	name: 'publicsubnet'
-			// }]
-		})
-
-		const postgres = new rds.DatabaseInstance(this, 'Postgres', {
-			engine: rds.DatabaseInstanceEngine.POSTGRES,
-			instanceClass: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
-			masterUsername: 'postgres',
-			vpc,
-			// vpcPlacement: {subnetType: ec2.SubnetType.PUBLIC}
-		})
 	}
 }
