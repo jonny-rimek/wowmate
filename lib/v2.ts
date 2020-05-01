1183e78eba9d79bed482ccf1c9ab65263b6ed471
Jonny Rimek <jimbo.db@protonmail.com>
2020-05-01 18:55:52 +0200

    Add Gin API and cdk fargate service

--- lib/v2.ts
+++ lib/v2.ts
@@ -1,6 +1,8 @@
 import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
 import rds = require('@aws-cdk/aws-rds');
+import ecs = require('@aws-cdk/aws-ecs');
+import ecsPatterns = require('@aws-cdk/aws-ecs-patterns');
 
 export class V2 extends cdk.Construct {
 	constructor(scope: cdk.Construct, id: string) {
@@ -24,5 +26,15 @@ export class V2 extends cdk.Construct {
 		})
 
 		postgres.connections.allowFromAnyIpv4(ec2.Port.tcp(5432))
+
+		const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+		vpc,
+		memoryLimitMiB: 512,
+		cpu: 256,
+		desiredCount: 1,
+		taskImageOptions: {
+			image: ecs.ContainerImage.fromAsset('services/api'),
+		},
+		});
 	}
 }
