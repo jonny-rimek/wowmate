dce3b11b5c276e926d99c3e46ab93157f5250bf3
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-13 23:17:44 +0200

    Drop pipeline construct

--- bin/wowmate.ts
+++ bin/wowmate.ts
@@ -1,54 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import { V2 } from '../lib/v2';
-import { Construct, Stage, Stack, StackProps, StageProps, SecretValue } from '@aws-cdk/core';
-import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
-
-class Wowmate extends Stage {
-	constructor(scope: Construct, id: string, props?: StageProps) {
-		super(scope, id, props);
-
-		new V2(this, 'V2')
-		// new Frontend(this, 'frontend')
-	}
-}
-
-class WowmatePipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    const sourceArtifact = new codepipeline.Artifact();
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
-
-    const pipeline = new CdkPipeline(this, 'Pipeline', {
-      pipelineName: 'WowmatePipeline',
-      cloudAssemblyArtifact,
-
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
-		actionName: 'GitHub',
-		output: sourceArtifact,
-		branch: 'master',
-		oauthToken: SecretValue.secretsManager('github-personal-access-token'),
-		//TODO: switch to webhook, might need to update the oath token
-        trigger: codepipeline_actions.GitHubTrigger.POLL,
-        owner: 'jonny-rimek',
-        repo: 'wowmate',
-      }),
-      synthAction: SimpleSynthAction.standardNpmSynth({
-        sourceArtifact,
-        cloudAssemblyArtifact,
-
-        buildCommand: 'npm run build',
-      }),
-    });
-
-	const wmp = pipeline.addApplicationStage(new Wowmate(this, 'Prod'));
-	wmp.addManualApprovalAction();
-  }
-}
+import { Wowmate } from '../lib/wowmate-stack';
 
 /*
 TODO: add 
@@ -60,6 +13,6 @@ constructor(scope: Construct, id: string, props?: EnvProps) {
 	*/
 
 const app = new cdk.App();
-new WowmatePipelineStack(app, 'WoWM', {
+new Wowmate(app, 'WoWM', {
 	env: {region: "us-east-1"}
 });
--- lib/wowmate-stack.ts
+++ lib/wowmate-stack.ts
@@ -5,47 +5,13 @@ import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
 
-export class Wowmate extends Stage {
+export class Wowmate extends Stack {
 	constructor(scope: Construct, id: string, props?: StageProps) {
 		super(scope, id, props);
 
 		new V2(this, 'V2')
 		// new Frontend(this, 'frontend')
 	}
-}
-
-export class WowmatePipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    const sourceArtifact = new codepipeline.Artifact();
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
-
-    const pipeline = new CdkPipeline(this, 'Pipeline', {
-      pipelineName: 'WowmatePipeline',
-      cloudAssemblyArtifact,
-
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
-		actionName: 'GitHub',
-		output: sourceArtifact,
-		branch: 'pipelines',
-		oauthToken: SecretValue.secretsManager('github-personal-access-token'),
-		//TODO: switch to webhook, might need to update the oath token
-        trigger: codepipeline_actions.GitHubTrigger.POLL,
-        owner: 'jonny-rimek',
-        repo: 'wowmate',
-      }),
-      synthAction: SimpleSynthAction.standardNpmSynth({
-        sourceArtifact,
-        cloudAssemblyArtifact,
-
-        buildCommand: 'npm run build',
-      }),
-    });
-
-	const wmp = pipeline.addApplicationStage(new Wowmate(this, 'Prod'));
-	wmp.addManualApprovalAction();
-  }
 }
 /* 
 export class Wowmate extends cdk.Stack {
