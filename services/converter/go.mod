4d62dde05b2ebcb3e5c859121445a53f4d074b8d
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-17 23:18:40 +0200

    Add polling SQS
     #6

--- services/converter/go.mod
+++ services/converter/go.mod
@@ -2,4 +2,7 @@ module github.com/jonny-rimek/wowmate/services/converter
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/aws/aws-sdk-go v1.34.6
+	github.com/gin-gonic/gin v1.6.3
+)
--- services/converter/main.go
+++ services/converter/main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 func main() {
+	queueURL := os.Getenv("QUEUE_URL")
+	log.Println(queueURL)
+
 	for {
 		log.Println("lol")
-		time.Sleep(time.Duration(1)*time.Minute)
+		time.Sleep(time.Duration(10)*time.Second)
+
+		sess,_ := session.NewSession()
+		//TODO: check and handle error
+		svc := sqs.New(sess)
+		
+		msgResult, _ := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+			QueueUrl: aws.String(queueURL),
+		})
+
+		log.Println(msgResult.Messages[0].Body)
 	}
 }
 
