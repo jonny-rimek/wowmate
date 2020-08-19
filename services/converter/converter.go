3600bddc137b2c9830bf6022267bb6b268fc0ddb
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-19 02:32:56 +0200

    Add s3 upload draft and update ci
     #6

--- services/converter/converter.go
+++ services/converter/converter.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 func main() {
 	queueURL := os.Getenv("QUEUE_URL")
+	bucket := os.Getenv("BUCKET_NAME")
+	key := os.Getenv("FILE_NAME")
 	log.Println(queueURL)
 
 	for {
@@ -28,9 +33,25 @@ func main() {
 
 		if len(msgResult.Messages) > 0 {
 			log.Println(*msgResult.Messages[0].Body)
-			//TODO: download from s3
+			downloader := s3manager.NewDownloader(sess)
 
-			_, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
+			file := &aws.WriteAtBuffer{}
+
+			//bytes unused
+			_, err := downloader.Download(
+				file,
+				&s3.GetObjectInput{
+					Bucket: aws.String(bucket),
+					Key:    aws.String(key),
+				},
+			)
+
+			if err != nil {
+				fmt.Printf("Unable to download item %v from bucket %v: %v", key, bucket, err)
+				return
+			}
+
+			_, err = svc.DeleteMessage(&sqs.DeleteMessageInput{
 				QueueUrl:      aws.String(queueURL),
 				ReceiptHandle: msgResult.Messages[0].ReceiptHandle,
 			})
--- services/converter/go.mod
+++ services/converter/go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.34.6
-	github.com/gin-gonic/gin v1.6.3
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
