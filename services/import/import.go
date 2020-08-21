a0f844a3b4b56bf206ebd981a3c987e7c348b9dd
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-22 00:54:13 +0200

    Fix connecting to db fromn import lambda #7

--- services/import/import.go
+++ services/import/import.go
@@ -22,7 +22,7 @@ type DatabasesCredentials struct {
 	Host         string `json:"host"`
 }
 
-func handle() {
+func handle() error {
 	secretArn := os.Getenv("SECRET_ARN")
 	_ = os.Getenv("CSV_BUCKET_NAME")
 
@@ -30,10 +30,12 @@ func handle() {
 
 	sess, err := session.NewSession()
 	if err != nil {
+		fmt.Println(err.Error())
 		log.Println("failed to create new session")
-		return
+		return err
 	}
 
+	//TODO: should move get secret outside of handler, because it dosn't needto run on every invocation
 	svc := secretsmanager.New(sess)
 
 	input := &secretsmanager.GetSecretValueInput{
@@ -48,63 +50,72 @@ func handle() {
 			case secretsmanager.ErrCodeDecryptionFailure:
 				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
 				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
-				return
+				return err
 
 			case secretsmanager.ErrCodeInternalServiceError:
 				// An error occurred on the server side.
 				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
-				return
+				return err
 
 			case secretsmanager.ErrCodeInvalidParameterException:
 				// You provided an invalid value for a parameter.
 				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
-				return
+				return err
 
 			case secretsmanager.ErrCodeInvalidRequestException:
 				// You provided a parameter value that is not valid for the current state of the resource.
 				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
-				return
+				return err
 
 			case secretsmanager.ErrCodeResourceNotFoundException:
 				// We can't find the resource that you asked for.
 				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
-				return
+				return err
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
 			fmt.Println(err.Error())
-			return
+			return err
 		}
 	}
+
+	log.Println(*result.SecretString)
 
 	var creds = DatabasesCredentials{}
 	err = json.Unmarshal([]byte(*result.SecretString), &creds)
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return err
 	}
 	//TODO: get connecetion data from secretsmanager
 	connStr := fmt.Sprintf(
-		"user=%v dbname=%v sslmode=%v host=verify-full password=%v",
+		"user=%v dbname=%v sslmode=verify-full host=%v password=%v port=5432",
 		creds.UserName,
 		creds.DatabaseName,
 		creds.Host,
 		creds.Password,
 	)
 
+	log.Println(connStr)
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
+		return err
 	}
+	log.Println("openend connection")
 
 	_, err = db.Query(`
-			SHOW TABLES;
+			SELECT 1;
 		`)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
+		return err
 	}
 	log.Println("finished")
+
+	return nil
 }
 
 func main() {
