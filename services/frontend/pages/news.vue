2540c85033e8cc188944ef39b2bba40b03f2ea1a
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-25 17:36:10 +0200

    Add tailwind ui blog snippet #22

--- services/frontend/pages/news.vue
+++ services/frontend/pages/news.vue
@@ -1,5 +1,137 @@
 <template>
-  <h1>news</h1>
+  <div>
+    <div class="pt-8 pb-20 px-4 sm:px-6 lg:pt-12 lg:pb-28 lg:px-8">
+      <div class="relative max-w-lg mx-auto lg:max-w-7xl">
+        <div>
+          <h2
+            class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10"
+          >
+            Press
+          </h2>
+          <div
+            class="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center"
+          >
+            <p class="text-xl leading-7 text-gray-500">
+              Get weekly articles in your inbox on how to grow your business.
+            </p>
+            <form class="mt-6 flex lg:mt-0 lg:justify-end">
+              <input
+                aria-label="Email address"
+                type="email"
+                required
+                class="appearance-none w-full px-4 py-2 border border-gray-300 text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out lg:max-w-xs"
+                placeholder="Enter your email"
+              />
+              <span class="ml-3 flex-shrink-0 inline-flex rounded-md shadow-sm">
+                <button
+                  type="button"
+                  class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+                >
+                  Notify me
+                </button>
+              </span>
+            </form>
+          </div>
+        </div>
+        <div
+          class="mt-6 grid gap-16 border-t-2 border-gray-100 pt-10 lg:grid-cols-2 lg:col-gap-5 lg:row-gap-12"
+        >
+          <div>
+            <p class="text-sm leading-5 text-gray-500">
+              <time datetime="2020-03-16">Mar 16, 2020</time>
+            </p>
+            <a href="#" class="block">
+              <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                Boost your conversion rate
+              </h3>
+              <p class="mt-3 text-base leading-6 text-gray-500">
+                Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+                exercitationem placeat consectetur nulla deserunt vel. Iusto
+                corrupti dicta.
+              </p>
+            </a>
+            <div class="mt-3">
+              <a
+                href="#"
+                class="text-base leading-6 font-semibold text-red-600 hover:text-indigo-500 transition ease-in-out duration-150"
+              >
+                Read full story
+              </a>
+            </div>
+          </div>
+          <div>
+            <p class="text-sm leading-5 text-gray-500">
+              <time datetime="2020-03-10">Mar 10, 2020</time>
+            </p>
+            <a href="#" class="block">
+              <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                How to use search engine optimization to drive sales
+              </h3>
+              <p class="mt-3 text-base leading-6 text-gray-500">
+                Optio cum necessitatibus dolor voluptatum provident commodi et.
+                Qui aperiam fugiat nemo cumque.
+              </p>
+            </a>
+            <div class="mt-3">
+              <a
+                href="#"
+                class="text-base leading-6 font-semibold text-red-600 hover:text-indigo-500 transition ease-in-out duration-150"
+              >
+                Read full story
+              </a>
+            </div>
+          </div>
+          <div>
+            <p class="text-sm leading-5 text-gray-500">
+              <time datetime="2020-02-12">Feb 12, 2020</time>
+            </p>
+            <a href="#" class="block">
+              <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                Improve your customer experience
+              </h3>
+              <p class="mt-3 text-base leading-6 text-gray-500">
+                Cupiditate maiores ullam eveniet adipisci in doloribus nulla
+                minus. Voluptas iusto libero adipisci rem et corporis.
+              </p>
+            </a>
+            <div class="mt-3">
+              <a
+                href="#"
+                class="text-base leading-6 font-semibold text-red-600 hover:text-indigo-500 transition ease-in-out duration-150"
+              >
+                Read full story
+              </a>
+            </div>
+          </div>
+          <div>
+            <p class="text-sm leading-5 text-gray-500">
+              <time datetime="2020-01-29">Jan 29, 2020</time>
+            </p>
+            <a href="#" class="block">
+              <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                Writing effective landing page copy
+              </h3>
+              <p class="mt-3 text-base leading-6 text-gray-500">
+                Ipsum voluptates quia doloremque culpa qui eius. Id qui id
+                officia molestias quaerat deleniti. Qui facere numquam autem
+                libero quae cupiditate asperiores vitae cupiditate. Cumque id
+                deleniti explicabo.
+              </p>
+            </a>
+            <div class="mt-3">
+              <a
+                href="#"
+                class="text-base leading-6 font-semibold text-red-600 hover:text-indigo-500 transition ease-in-out duration-150"
+              >
+                Read full story
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
