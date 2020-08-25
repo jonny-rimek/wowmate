14ba9d261ec45968d1d7fa2095edba0b83fea50c
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-25 23:34:22 +0200

    Switch to other svg on landing page

--- services/frontend/pages/index.vue
+++ services/frontend/pages/index.vue
@@ -1,47 +1,5 @@
 <template>
   <div>
-    <!--
-    <svg
-      class="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
-      width="640"
-      height="784"
-      fill="none"
-      viewBox="0 0 640 784"
-    >
-      <defs>
-        <pattern
-          id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
-          x="118"
-          y="0"
-          width="20"
-          height="20"
-          patternUnits="userSpaceOnUse"
-        >
-          <rect
-            x="0"
-            y="0"
-            width="4"
-            height="4"
-            class="text-gray-200"
-            fill="currentColor"
-          />
-        </pattern>
-      </defs>
-      <rect
-        y="72"
-        width="640"
-        height="640"
-        class="text-gray-50"
-        fill="currentColor"
-      />
-      <rect
-        x="118"
-        width="404"
-        height="784"
-        fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"
-      />
-    </svg>
-	-->
     <!-- TODO: add nice patterns -->
     <main
       class="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24"
@@ -100,15 +58,15 @@
         <div
           class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center"
         >
-          <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+          <div class="relative mx-auto w-full rounded-lg lg:max-w-md">
             <button
               type="button"
               class="relative block w-full rounded-lg overflow-hidden focus:outline-none focus:shadow-outline"
             >
               <img
                 class="w-full"
-                src="undraw_financial_data_es63.png"
-                alt="Woman making a sale"
+                src="undraw_dark_analytics_7axy.svg"
+                alt="graph"
               />
               <div
                 class="absolute inset-0 w-full h-full flex items-center justify-center"
