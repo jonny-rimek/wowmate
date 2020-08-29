abd076f42b2940dd4b620f595ea759dcced2e4c6
Jonny Rimek <jimbo.db@protonmail.com>
2020-08-29 02:56:16 +0200

    Improve m+ sidebar, add option for all dungeons
    and add exact active class

--- services/frontend/components/SidebarLink.vue
+++ services/frontend/components/SidebarLink.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link
     :to="{ name: pathName, params: { id: param } }"
-    active-class="text-gray-700 bg-gray-200 dark:bg-gray-700 dark:text-gray-200 font-bold"
+    exact-active-class="text-gray-700 bg-gray-200 dark:bg-gray-700 dark:text-gray-200 font-bold"
     class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium dark:text-gray-300 text-gray-500 hover:bg-gray-200 dark-hover:bg-gray-700 dark-hover:text-gray-300 hover:text-gray-700 rounded-md focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150"
   >
     <span class="truncate">
--- services/frontend/pages/mythicplus/mobile.vue
+++ services/frontend/pages/mythicplus/mobile.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="p-5 lg:hidden">
-    <SidebarLink pathName="mythicplus">
-      All Dungeons
-    </SidebarLink>
     <SidebarLink
       v-for="item in dungeons"
       :param="item.id"
-      pathName="mythicplus-id"
+      :pathName="item.pathName"
       :key="item.id"
     >
       {{ item.name }}
@@ -25,18 +22,27 @@ export default {
   data() {
     return {
       dungeons: [
-        { name: "Atal'Dazar", id: 2144 },
-        { name: 'Freehold', id: 2145 },
-        { name: "Kings'Rest", id: 2146 },
-        { name: 'Shrine of the Storm', id: 2147 },
-        { name: 'Siege of Boralus', id: 2148 },
-        { name: 'Temple of Sethraliss', id: 2149 },
-        { name: 'The MOTHERLODE!!', id: 2154 },
-        { name: 'The Underrot', id: 2164 },
-        { name: 'Tol Dagor', id: 2174 },
-        { name: 'Waycrest Manor', id: 2184 },
-        { name: 'Operation: Mechagon - Junkyard', id: 2194 },
-        { name: 'Operation: Mechagon - Workshop', id: 2104 }
+        { name: 'All dungeons', pathName: 'mythicplus' },
+        { name: "Atal'Dazar", pathName: 'mythicplus-id', id: 2144 },
+        { name: 'Freehold', pathName: 'mythicplus-id', id: 2145 },
+        { name: "Kings'Rest", pathName: 'mythicplus-id', id: 2146 },
+        { name: 'Shrine of the Storm', pathName: 'mythicplus-id', id: 2147 },
+        { name: 'Siege of Boralus', pathName: 'mythicplus-id', id: 2148 },
+        { name: 'Temple of Sethraliss', pathName: 'mythicplus-id', id: 2149 },
+        { name: 'The MOTHERLODE!!', pathName: 'mythicplus-id', id: 2154 },
+        { name: 'The Underrot', pathName: 'mythicplus-id', id: 2164 },
+        { name: 'Tol Dagor', pathName: 'mythicplus-id', id: 2174 },
+        { name: 'Waycrest Manor', pathName: 'mythicplus-id', id: 2184 },
+        {
+          name: 'Operation: Mechagon - Junkyard',
+          pathName: 'mythicplus-id',
+          id: 2194
+        },
+        {
+          name: 'Operation: Mechagon - Workshop',
+          pathName: 'mythicplus-id',
+          id: 2104
+        }
       ]
     }
   }
