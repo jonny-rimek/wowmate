d6ebdb67d2476f364dc2828ac953f0de69aab14a
Jonny Rimek <jimbo.db@protonmail.com>
2020-09-27 01:06:40 +0200

    Remove ded imports

--- lib/frontend/frontend.ts
+++ lib/frontend/frontend.ts
@@ -1,25 +1,14 @@
 import cdk = require('@aws-cdk/core');
-import ddb = require('@aws-cdk/aws-dynamodb');
 import s3 = require('@aws-cdk/aws-s3');
-import lambda = require('@aws-cdk/aws-lambda');
-import sfn = require('@aws-cdk/aws-stepfunctions');
-import tasks = require('@aws-cdk/aws-stepfunctions-tasks');
-import { RemovalPolicy, Duration } from '@aws-cdk/core';
-import { BlockPublicAccess } from '@aws-cdk/aws-s3';
-// import targets = require('@aws-cdk/aws-events-targets');
 import targets = require('@aws-cdk/aws-route53-targets');
 import iam = require('@aws-cdk/aws-iam');
-import { Effect } from '@aws-cdk/aws-iam';
 import cloudtrail = require('@aws-cdk/aws-cloudtrail');
 import apigateway = require('@aws-cdk/aws-apigateway');
 import s3deploy = require('@aws-cdk/aws-s3-deployment');
 import cloudfront = require('@aws-cdk/aws-cloudfront');
 import route53= require('@aws-cdk/aws-route53');
 import acm = require('@aws-cdk/aws-certificatemanager');
-import { SSLMethod, SecurityPolicyProtocol, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront';
-import { StateMachineType } from '@aws-cdk/aws-stepfunctions';
-import events = require('@aws-cdk/aws-events');
-import { CfnClientCertificate } from '@aws-cdk/aws-apigateway';
+import { SSLMethod, SecurityPolicyProtocol } from '@aws-cdk/aws-cloudfront';
 import { HttpApi } from '@aws-cdk/aws-apigatewayv2';
 
 interface Props extends cdk.StackProps {
