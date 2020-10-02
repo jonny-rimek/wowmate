53eef2f1c3811241a29eafc7162dd28898cc45c6
Jonny Rimek <jimbo.db@protonmail.com>
2020-10-02 16:26:39 +0200

    Return nat gateways

--- lib/common/vpc.ts
+++ lib/common/vpc.ts
@@ -3,19 +3,23 @@ import ec2 = require('@aws-cdk/aws-ec2');
 
 export class Vpc extends cdk.Construct {
 	public readonly vpc: ec2.Vpc;
+	public readonly natGateways: ec2.GatewayConfig[];
 
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id)
 
+		const nat = ec2.NatProvider.gateway()
+		this.natGateways = nat.configuredGateways
+
 		this.vpc = new ec2.Vpc(this, 'Vpc', {
 			natGateways: 1,
+			natGatewayProvider: nat,
 			//gateway endpoint will lead to no datatransfer cost for s3 to vpc/NATgateway
 			gatewayEndpoints: {
 				S3: {
 					service: ec2.GatewayVpcEndpointAwsService.S3,
 				}
 			}
-			
 		})
 	}
 }
