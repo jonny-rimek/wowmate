9e6e490752e74dd270f22370707368537f1c8548
Jonny Rimek <jimbo.db@protonmail.com>
2020-10-02 03:10:55 +0200

    Extract fileending and keep it on upload

--- services/upload/presign/index.js
+++ services/upload/presign/index.js
@@ -5,20 +5,40 @@ const s3 = new AWS.S3({signatureVersion: 'v4'});
 const uuidv4 = require('uuid/v4');
 
 exports.handler = (event, context, callback) => {
-    const bucket = process.env['BUCKET_NAME'];
-    if (!bucket) {
-        callback(new Error(`S3 bucket not set`));
-    }
-	console.log(bucket);
+	const bucket = process.env['BUCKET_NAME'];
+	if (!bucket) {
+		callback(new Error(`S3 bucket not set`));
+	}
 
-	console.log(event.requestContext.http.path);
-	
-    const key = uuidv4() + '.txt';
-    const params = {
-        'Bucket': bucket,
-        'Key': key,
-        'Content-Type': '',
-    };
+	const path = event.requestContext.http.path;
+
+	let fileending
+	let filesize
+
+	if (path.match(/\.txt.gz/) == '.txt.gz') {
+		fileending = '.txt.gz';
+		filesize = 31457280; 
+		//ziped files are smaller, but we still can't process larger files in the lambda
+		//once it's uncompressed
+	} else if (path.match(/\.txt/) == '.txt') {
+		fileending = '.txt';
+		filesize = 314572800;
+	} else if (path.match(/\.zip/) == '.zip') {
+		fileending = '.zip';
+		filesize = 31457280;
+	} else {
+		callback(null, {
+			statusCode: 500,
+			headers: {
+			"Access-Control-Allow-Origin": "*"
+			},
+			body: 'invalid filename',
+		});
+	}
+
+	console.log(fileending);
+
+	const key = uuidv4() + fileending;
 
 	//TODO: path to bucket name year/month/day/uuid.FILEENDING
 	const res = s3.createPresignedPost({
@@ -27,8 +47,8 @@ exports.handler = (event, context, callback) => {
 			key: key,
 		},
 		Conditions: [
-			["content-length-range", 	0, 314572800], // content length restrictions: 0-300 MB
-			//["starts-with", "$Content-Type", "image/"], // content type restriction
+			["content-length-range", 	0, filesize], // content length restrictions: 0-300 MB
+			//["starts-with", "$Content-Type", "image/"], // can't really use it because content might be ziped or text
 			{'success_action_status': '201'},
 			['starts-with', '$Content-Type', ''],
 			['starts-with', '$key', ''],
