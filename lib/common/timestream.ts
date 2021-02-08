16c9138e81da4443420611058598b9f1f77b3c73
Jonny Rimek <jimbo.db@protonmail.com>
2021-02-08 03:36:39 +0100

    Update timestream db ids/names

--- lib/common/timestream.ts
+++ lib/common/timestream.ts
@@ -8,6 +8,13 @@ interface Props extends cdk.StackProps {
 export class Timestream extends cdk.Construct {
 	constructor(scope: cdk.Construct, id: string, props?: Props) {
 		super(scope, id)
-		new timestream.CfnDatabase(this, "timestream")
+		new timestream.CfnDatabase(this, "db", {
+			databaseName: "wowmate-analytics",
+		})
+
+		new timestream.CfnTable(this, "table", {
+			databaseName: "wowmate-analytics",
+			tableName: "combatlogs",
+		})
 	}
 }
