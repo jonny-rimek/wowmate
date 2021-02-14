a1916d99be3fd22638f72e8ca13e486a6e8c1427
Jonny Rimek <jimbo.db@protonmail.com>
2021-02-14 13:34:19 +0100

    Batching up records before uploading to timestream

--- services/upload/convert/normalize/normalize.go
+++ services/upload/convert/normalize/normalize.go
@@ -2,18 +2,26 @@ package normalize
 
 import (
 	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/timestreamwrite"
+	"golang.org/x/net/http2"
 )
 
 //Normalize converts the combatlog to a slice of Event structs
 func Normalize(scanner *bufio.Scanner, uploadUUID string, sess *session.Session) error {
-	var combatEvents []Event
+	var combatEvents []*timestreamwrite.Record
 	//IMPROVE: the UploadUUID logic should be part of the normalize package
 	//UploadUUID //for the whole file
 	// CombatlogUUID := "" //after every COMBAT_LOG_VERSION
 	// BossFightUUID := ""
-	MythicplusUUID := ""
+	// MythicplusUUID := ""
 
 	//combatEvents = make([]Event, 0, 100000) //100.000 is an arbitrary value
 	//initialising the slice with a capacitiy to reduce the amount reallocations
@@ -31,34 +39,36 @@ func Normalize(scanner *bufio.Scanner, uploadUUID string, sess *session.Session)
 		//maybe there is a package that implements string functionality more efficient
 		//the main problem is that the strings.Split calls a bunch of other functions
 		//that all create a new version of the string and thus bloating the memory
-		//UPDATE: it's a lot of work to rewrite everything touse []byte I'll resist the
+		//UPDATE: it's a lot of work to rewrite everything to use []byte I'll resist the
 		//premature optimization for now
 		row := splitString(scanner.Text(), "  ")
 
 		//NOTE: not written to DB atm https://github.com/jonny-rimek/wowmate/issues/129
-		timestamp, err := timestampMilli(row[0])
-		if err != nil {
-			return err
-		}
+		//gonna use it later again
+		// timestamp, err := timestampMilli(row[0])
+		// if err != nil {
+		// 	return err
+		// }
 
 		params := splitAtCommas(&row[1])
-
-		e := Event{
-			// UploadUUID:     uploadUUID,
-			// CombatlogUUID:  CombatlogUUID,
-			// BossFightUUID:  BossFightUUID,
-			MythicplusUUID: MythicplusUUID,
-			// ColumnUUID:     uuid.Must(uuid.NewV4()).String(),
-			Timestamp: timestamp,
-			// EventType:      params[0],
-		}
+		
+		e := &timestreamwrite.Record{}
+		// e := Event{
+		// 	// UploadUUID:     uploadUUID,
+		// 	// CombatlogUUID:  CombatlogUUID,
+		// 	// BossFightUUID:  BossFightUUID,
+		// 	MythicplusUUID: MythicplusUUID,
+		// 	// ColumnUUID:     uuid.Must(uuid.NewV4()).String(),
+		// 	Timestamp: timestamp,
+		// 	// EventType:      params[0],
+		// }
 
 		//TODO: never add anything if CombatlogUUID is empty, same logic as m+uuid
 
-		if MythicplusUUID == "" && params[0] != "CHALLENGE_MODE_START" {
-			//I don't want to add events if they are outside of a combatlog
-			continue
-		}
+		// if MythicplusUUID == "" && params[0] != "CHALLENGE_MODE_START" {
+		//I don't want to add events if they are outside of a combatlog
+		// 	continue
+		// }
 
 		switch params[0] {
 		case "COMBAT_LOG_VERSION":
@@ -107,24 +117,25 @@ func Normalize(scanner *bufio.Scanner, uploadUUID string, sess *session.Session)
 			// if err != nil {
 			// 	return err
 			// }
-			combatEvents = append(combatEvents, e)
+			// combatEvents = append(combatEvents, e)
 
 			// r, err := convertToCSV(&combatEvents)
-			if err != nil {
-				return err
-			}
+			// if err != nil {
+			// 	return err
+			// }
 			// err = uploadS3(&r, sess, e.MythicplusUUID, csvBucket)
 			// if err != nil {
 			// 	return err
 			// }
 
-			combatEvents = nil
-			MythicplusUUID = ""
+			// combatEvents = nil
+			// MythicplusUUID = ""
 
 		// case "SPELL_HEAL", "SPELL_PERIODIC_HEAL":
 		// 	err = e.importHeal(params)
 		case "SPELL_DAMAGE":
-			err = e.spellDamage(params)
+			log.Println("inside spell damage")
+			err := spellDamage(e, params)
 			if err != nil {
 				return err
 			}
@@ -138,5 +149,54 @@ func Normalize(scanner *bufio.Scanner, uploadUUID string, sess *session.Session)
 		}
 		combatEvents = append(combatEvents, e)
 	}
+
+	err := uploadToTimestream(combatEvents)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func uploadToTimestream(e []*timestreamwrite.Record) error {
+	tr := &http.Transport{
+		ResponseHeaderTimeout: 20 * time.Second,
+		// Using DefaultTransport values for other parameters: https://golang.org/pkg/net/http/#RoundTripper
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+			Timeout:   30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	// So client makes HTTP/2 requests
+	err := http2.ConfigureTransport(tr)
+	if err != nil {
+		return err
+	} 
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1"), MaxRetries: aws.Int(10), HTTPClient: &http.Client{Transport: tr}})
+	if err != nil {
+		return err
+	} 
+
+	writeSvc := timestreamwrite.New(sess)
+	writeRecordsInput := &timestreamwrite.WriteRecordsInput{
+		DatabaseName: aws.String("wowmate-analytics"),
+		TableName:    aws.String("combatlogs"),
+		Records: e,
+	}
+
+	_, err = writeSvc.WriteRecords(writeRecordsInput)
+	if err != nil {
+		return err
+	} else {
+		fmt.Println("Write records is successful")
+		return nil
+	}
 }
