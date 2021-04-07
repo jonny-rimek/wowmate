86b438b1f1db958c9277b0fa01694022b469f12a
Jonny Rimek <jimbo.db@protonmail.com>
2021-04-07 10:30:37 +0200

    Add finished to timestream

--- services/upload/convert/normalize/challenge_mode_end.go
+++ services/upload/convert/normalize/challenge_mode_end.go
@@ -10,10 +10,17 @@ import (
 )
 
 //1/24 17:23:07.443  CHALLENGE_MODE_END,2291,1,10,2136094
-//not sure what the 1 stands for, maybe finished or timed
+//1/24 16:47:37.880  CHALLENGE_MODE_END,2291,0,0,0
+//if it's not a finish [2] is 0 and the other two aswell
 func challengeModeEnd(params []string, uploadUUID string, combatlogUUID string) ([]*timestreamwrite.Record, error) {
 	if len(params) != 5 {
 		return nil, fmt.Errorf("combatlog version should have 5 columns, it has %v: %v", len(params), params)
+	}
+
+	finished, err := Atoi64(params[2]) //2136094
+	if err != nil {
+		log.Printf("failed to convert challange mode end event, field finished. got: %v", params[2])
+		return nil, err
 	}
 
 	duration, err := Atoi64(params[4]) //2136094
@@ -38,6 +45,23 @@ func challengeModeEnd(params []string, uploadUUID string, combatlogUUID string) 
 			},
 			MeasureName:      aws.String("duration"),
 			MeasureValue:     aws.String(strconv.FormatInt(duration, 10)),
+			MeasureValueType: aws.String("BIGINT"),
+			Time:             aws.String(strconv.FormatInt(currentTimeInSeconds, 10)),
+			TimeUnit:         aws.String("SECONDS"),
+		},
+		{
+			Dimensions: []*timestreamwrite.Dimension{
+				{
+					Name:  aws.String("upload_uuid"),
+					Value: aws.String(uploadUUID),
+				},
+				{
+					Name:  aws.String("combatlog_uuid"),
+					Value: aws.String(combatlogUUID),
+				},
+			},
+			MeasureName:      aws.String("finished"),
+			MeasureValue:     aws.String(strconv.FormatInt(finished, 10)),
 			MeasureValueType: aws.String("BIGINT"),
 			Time:             aws.String(strconv.FormatInt(currentTimeInSeconds, 10)),
 			TimeUnit:         aws.String("SECONDS"),
