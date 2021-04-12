18791f92d12ea114aa279a4f4d035f07a38e102a
Jonny Rimek <jimbo.db@protonmail.com>
2021-04-12 17:54:18 +0200

    Remove spellType because we don't need it

--- services/upload/convert/normalize/spell_damage.go
+++ services/upload/convert/normalize/spell_damage.go
@@ -19,31 +19,37 @@ import (
 //
 // 1/24 16:47:51.662  SPELL_DAMAGE,Player-581-04A01EDA,"Ayléén-Blackrock",0x511,0x0,Creature-0-4234-2291-11942-168992-00000DA4A7,"Risen Cultist",0x10a48,0x0,50842,"Blood Boil", 0x20,Creature-0-4234-2291-11942-168992-00000DA4A7,0000000000000000,177038,177822,0,0,1071,0,0,2434,2434,0,2943.32,-2219.79,1680,3.6342,60,784,783,-1,32,0,0,0,nil,nil,nil
 // 1/24 16:48:17.916  SPELL_PERIODIC_DAMAGE,Player-3674-0906D09A,"Bihla-TwistingNether",0x512,0x0,Creature-0-4234-2291-11942-168992-00020DA4A7,"Risen Cultist",0xa48,0x0,204213,"Purge the Wicked",0x4,Creature-0-4234-2291-11942-168992-00020DA4A7,0000000000000000,748,177822,0,0,1071,0,0,2434,2434,0,2903.19,-2224.57,1680,4.6220,60,483,241,-1,4,0,0,0,1,nil,nil
-// 1/24 16:48:22.569  RANGE_DAMAGE,Player-1403-09B9285B,"Luminal-Draenor",0x512,0x0,Creature-0-4234-2291-11942-174773-00008DA4CF,"Spiteful Shade",0xa48,0x0,75,"Auto Shot",0x1,Creature-0-       4234-2291-11942-174773-00008DA4CF,Creature-0-4234-2291-11942-168949-00010DA4A7,40914,88911,0,0,1071,0,1,0,0,0,2904.33,-2216.53,1680,1.2998,60,1295,924,-1,1,0,0,0,1,nil,nil
+// 1/24 16:48:22.569  RANGE_DAMAGE,Player-1403-09B9285B,"Luminal-Draenor",0x512,0x0,Creature-0-4234-2291-11942-174773-00008DA4CF,"Spiteful Shade",0xa48,0x0,75,"Auto Shot",0x1,Creature-0-4234-2291-11942-174773-00008DA4CF,Creature-0-4234-2291-11942-168949-00010DA4A7,40914,88911,0,0,1071,0,1,0,0,0,2904.33,-2216.53,1680,1.2998,60,1295,924,-1,1,0,0,0,1,nil,nil
+// 1/24 16:48:22.684  SWING_DAMAGE,Player-581-04A01EDA,"Ayléén-Blackrock",0x511,0x0,Creature-0-4234-2291-11942-169905-00010DA4A7,"Risen Warlord",0x10a48,0x0,Player-581-04A01EDA, 0000000000000000,45866,56060,1665,242,1918,0,6,543,1250,0,2901.84,-2241.36,1680,1.1696,204,791,1129,-1,1,0,0,0,nil,nil,nil
 // passing in the uuids as a pointer to the string reduced the mb usage by a couple MB
 func spellDamage(params []string, uploadUUID *string, combatlogUUID *string, rec map[string]map[string][]*timestreamwrite.WriteRecordsInput) error {
 	if len(params) != 39 {
 		return fmt.Errorf("combatlog version should have 39 columns, it has %v: %v", len(params), params)
 	}
 
-	actualAmount, err := Atoi64(params[29]) // 783
-	if err != nil {
-		log.Printf("failed to convert damage event, field actual amount. got: %v", params[29])
-		return err
-	}
+	var actualAmount, currentTimeInSeconds int64
+	var spellID int
+	var err error
+	var casterID, casterName, casterType, spellName string
 
-	spellID, err := strconv.Atoi(params[9]) // 50842
-	if err != nil {
-		log.Printf("failed to convert damage event, field spell id. got: %v", params[9])
-		return err
-	}
+	if params[0] != "SWING_DAMAGE" {
+		actualAmount, err = Atoi64(params[29]) // 783
+		if err != nil {
+			log.Printf("failed to convert damage event, field actual amount. got: %v", params[29])
+			return err
+		}
+		spellID, err = strconv.Atoi(params[9]) // 50842
+		if err != nil {
+			log.Printf("failed to convert damage event, field spell id. got: %v", params[9])
+			return err
+		}
 
-	casterID := params[1]
-	casterName := trimQuotes(params[2])
-	casterType := trimQuotes(params[3])
-	spellName := trimQuotes(params[10])
-	spellType := params[11]
-	currentTimeInSeconds := time.Now().Unix()
+		casterID = params[1]
+		casterName = trimQuotes(params[2])
+		casterType = trimQuotes(params[3])
+		spellName = trimQuotes(params[10])
+		currentTimeInSeconds = time.Now().Unix()
+	}
 	rand.Seed(time.Now().UnixNano())
 
 	key := strconv.Itoa(spellID)
@@ -92,10 +98,6 @@ func spellDamage(params []string, uploadUUID *string, combatlogUUID *string, rec
 						{
 							Name:  aws.String("spell_name"),
 							Value: aws.String(spellName),
-						},
-						{
-							Name:  aws.String("spell_type"),
-							Value: aws.String(spellType),
 						},
 						{
 							Name:  aws.String("upload_uuid"),
@@ -156,10 +158,6 @@ func spellDamage(params []string, uploadUUID *string, combatlogUUID *string, rec
 					{
 						Name:  aws.String("spell_name"),
 						Value: aws.String(spellName),
-					},
-					{
-						Name:  aws.String("spell_type"),
-						Value: aws.String(spellType),
 					},
 					{
 						Name:  aws.String("upload_uuid"),
