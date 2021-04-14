d4c92ce42286b2d8c177f3d257778306a95baf70
Jonny Rimek <jimbo.db@protonmail.com>
2021-04-14 08:48:32 +0200

    Don't track pets not summoned by players

--- services/upload/convert/normalize/normalize.go
+++ services/upload/convert/normalize/normalize.go
@@ -151,6 +151,12 @@ type pet struct {
 
 // 1/24 16:50:53.696  SPELL_SUMMON,Player-3674-0906D09A,"Bihla-TwistingNether",0x512,0x0,Creature-0-4234-2291-11942-19668-00000DA56B,"Shadowfiend",0xa28,0x0,34433,"Shadowfiend",0x20
 func summon(params []string, uploadUUID *string, combatlogUUID *string, rec map[string]map[string][]*timestreamwrite.WriteRecordsInput, pets map[string]pet) error {
+	// only track player pets
+	casterType := trimQuotes(params[3])
+	if casterType != "0x512" && casterType != "0x511" {
+		return nil
+	}
+
 	if len(params) != 12 {
 		return fmt.Errorf("SPELL_SUMMON should have 13 columns, it has %v: %v", len(params), params)
 	}
@@ -165,6 +171,7 @@ func summon(params []string, uploadUUID *string, combatlogUUID *string, rec map[
 		Name:      trimQuotes(params[10]), // params[7] is the pet name and [11] is the spell name, in this case the same
 		SpellID:   spellID,
 	}
+
 	// prettyStruct, err := golib.PrettyStruct(pets)
 	// if err != nil {
 	// 	return err
