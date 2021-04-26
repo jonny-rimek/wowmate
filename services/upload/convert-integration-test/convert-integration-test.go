2e889203a90d1e577421d85a7b8ab3d3a60dc995
Jonny Rimek <jimbo.db@protonmail.com>
2021-04-26 21:04:11 +0200

    Add event for preprod integration test

--- services/upload/convert-integration-test/convert-integration-test.go
+++ services/upload/convert-integration-test/convert-integration-test.go
@@ -43,6 +43,26 @@ func invokeConvert(local bool) error {
 			}
 		`)
 	} else {
+		payload = []byte(`
+			{
+			  "Records": [
+				{
+				  "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+				  "receiptHandle": "MessageReceiptHandle",
+				  "body": "{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-east-1\",\"eventTime\":\"2020-09-03T22:04:40.595Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"AWS:AIDA5WEGR6IGFG5BIU7QW\"},\"requestParameters\":{\"sourceIPAddress\":\"37.120.217.169\"},\"responseElements\":{\"x-amz-request-id\":\"2F5A942F7BB7532F\",\"x-amz-id-2\":\"fjLaREBoWXqzZymVY2hclfWmjutD45WbiKWoxCBeGkYnESkJp+yaSiAy5WcTX9yaup1xERIJbXPUKLmQzg2tZHAS5v8cdcXI\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"NGJhMDAzOWEtNTliZS00NTY4LThhZjgtNjAyODc1NmJiMjQy\",\"bucket\":{\"name\":\"wm-preprod-bucketsupload0b7f8f15-14ny22audh3ba\",\"ownerIdentity\":{\"principalId\":\"AHRIC0SLDQ6UK\"},\"arn\":\"arn:aws:s3:::wm-bucketsupload0b7f8f15-1k8l1923idej5\"},\"object\":{\"key\":\"upload/2021/4/26/18/34d55f0c-fadf-4234-9c99-87a855f20ef0.zip\",\"size\":6873965,\"eTag\":\"cd1a984dd8323ef1e6e5541d363d5e4d\",\"sequencer\":\"005F5168797266420C\"}}}]}",
+				  "attributes": {
+					"ApproximateReceiveCount": "1",
+					"SentTimestamp": "1523232000000",
+					"SenderId": "123456789012",
+					"ApproximateFirstReceiveTimestamp": "1523232000001"
+				  },
+				  "eventSource": "aws:sqs",
+				  "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
+				  "awsRegion": "us-east-1"
+				}
+			  ]
+			}
+		`)
 		functionName = "wm-preprod-ConvertLambda3540DCCB-1898ZUCBEU62K"
 		logType = aws.String(lambdaService.LogTypeTail)
 	}
