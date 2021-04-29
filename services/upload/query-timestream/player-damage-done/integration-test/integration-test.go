51fdf3e63f38dc31cb3e43ed76522d11d8f9b47e
Jonny Rimek <jimbo.db@protonmail.com>
2021-04-29 03:22:17 +0200

    Create new integration test for query player dmg

---
+++ services/upload/query-timestream/player-damage-done/integration-test/integration-test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
+)
+
+var lambdaSvc *lambdaService.Lambda
+
+func invokeConvert(local bool) error {
+	var functionName string
+	var logType *string
+	var payload []byte
+	payload = nil
+
+	if local == true {
+		functionName = "" // TODO
+		logType = nil
+		payload = []byte(`
+		`) // TODO
+	} else {
+		payload = []byte(`
+		`)
+		functionName = "" // TODO
+		logType = aws.String(lambdaService.LogTypeTail)
+	}
+
+	input := &lambdaService.InvokeInput{
+		FunctionName:   &functionName,
+		Payload:        payload,
+		InvocationType: aws.String(lambdaService.InvocationTypeRequestResponse), // synchronous - default
+		// it needs to be synchronous so I can assert on the response if an error was returned or not
+		// even if in aws it's called async via SNS
+		LogType: logType, // returns the log in the response
+	}
+	err := input.Validate()
+	if err != nil {
+		return fmt.Errorf("validating the input failed: %s", err.Error())
+	}
+
+	resp, err := lambdaSvc.Invoke(input)
+	if err != nil {
+		return fmt.Errorf("failed to invoke lambda: %s", err.Error())
+	}
+	log.Printf("%s", resp.Payload)
+
+	if local != true {
+		// log doesn't work locally
+		decodeString, err := base64.StdEncoding.DecodeString(*resp.LogResult)
+		if err != nil {
+			return fmt.Errorf("failed to decode the log: %s", err.Error())
+		}
+		log.Printf("log result: %s", decodeString)
+	}
+
+	if resp.FunctionError != nil {
+		return fmt.Errorf("lambda was invoked but returned error: %s", *resp.FunctionError)
+	}
+	return nil
+}
+
+func main() {
+	local := true
+	// CI is always set to true if it runs in a github action, if the env is not present it's ran locally
+	if os.Getenv("CI") == "true" {
+		local = false
+	}
+	var sess *session.Session
+	var err error
+
+	if local == true {
+		sess, err = session.NewSession(
+			&aws.Config{
+				Endpoint:   aws.String("http://127.0.0.1:3001"),
+				DisableSSL: aws.Bool(true),
+				Region:     aws.String("us-east-1"),
+				MaxRetries: aws.Int(0),
+			})
+	} else {
+		sess, err = session.NewSession()
+	}
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	lambdaSvc = lambdaService.New(sess)
+
+	err = invokeConvert(local)
+	if err != nil {
+		log.Printf("%s", err)
+		os.Exit(1)
+	}
+	log.Println("invoke finished")
+}
