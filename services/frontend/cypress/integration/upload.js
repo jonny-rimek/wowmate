18639cf6683082df4b8717f5c8e05d9da40a6f5a
Jonny Rimek <jimbo.db@protonmail.com>
2021-05-03 15:16:49 +0200

    Add upload zip and gzip

--- services/frontend/cypress/integration/upload.js
+++ services/frontend/cypress/integration/upload.js
@@ -5,5 +5,11 @@ describe('Upload Page', () => {
     cy.fixture('otherside-empty.txt', 'base64').then(content => {
       cy.get('#dropzone').upload(content, 'otherside-empty.txt')
     })
+    cy.fixture('otherside-empty.txt.gz', 'base64').then(content => {
+      cy.get('#dropzone').upload(content, 'otherside-empty.txt.gz')
+    })
+    cy.fixture('otherside-empty.zip', 'base64').then(content => {
+      cy.get('#dropzone').upload(content, 'otherside-empty.zip')
+    })
   })
 })
