cf324b4d4a1aca8a133179198de43972686f96b1
Jonny Rimek <jimbo.db@protonmail.com>
2022-07-16 23:54:25 +0200

    Start over with ecs thingy

--- tf/networking.tf
+++ tf/networking.tf
@@ -2,9 +2,13 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
 resource "aws_subnet" "public" {
   count                   = 2
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2 + count.index)
+  cidr_block              = element(["10.0.0.0/24", "10.0.2.0/24"], count.index)
   availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
   vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
@@ -12,108 +16,53 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count             = 2
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  cidr_block        = element(["10.0.3.0/24", "10.0.4.0/24"], count.index)
   availability_zone = data.aws_availability_zones.available_zones.names[count.index]
   vpc_id            = aws_vpc.main.id
 }
 
-resource "aws_internet_gateway" "gateway" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.main.main_route_table_id
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gateway.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
-resource "aws_eip" "gateway" {
-  count      = 2
-  vpc        = true
-  depends_on = [aws_internet_gateway.gateway]
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_nat_gateway" "gateway" {
+resource "aws_nat_gateway" "main" {
   count         = 2
+  allocation_id = element(aws_eip.nat.*.id, count.index)
   subnet_id     = element(aws_subnet.public.*.id, count.index)
-  allocation_id = element(aws_eip.gateway.*.id, count.index)
+  depends_on    = [aws_internet_gateway.main]
+}
+
+resource "aws_eip" "nat" {
+  count = 2
+  vpc = true
 }
 
 resource "aws_route_table" "private" {
   count  = 2
   vpc_id = aws_vpc.main.id
+}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
-  }
+resource "aws_route" "private" {
+  count                  = 2
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
 }
 
 resource "aws_route_table_association" "private" {
   count          = 2
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
-}
-
-resource "aws_security_group" "lb" {
-  name        = "example-alb-security-group"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "default" {
-  name            = "example-lb"
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.lb.id]
-}
-
-resource "aws_lb_target_group" "hello_world" {
-  name        = "example-target-group"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-}
-
-resource "aws_lb_listener" "hello_world" {
-  load_balancer_arn = aws_lb.default.id
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_lb_target_group.hello_world.id
-    type             = "forward"
-  }
-}
-
-resource "aws_security_group" "hello_world_task" {
-  name        = "example-task-security-group"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    protocol        = "tcp"
-    from_port       = 3000
-    to_port         = 3000
-    security_groups = [aws_security_group.lb.id]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
--- tf/output.tf
+++
@@ -1,3 +0,0 @@
-output "load_balancer_ip" {
-  value = aws_lb.default.dns_name
-}
