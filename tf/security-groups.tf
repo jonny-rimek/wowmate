90b36ab3f873a3ded664797b46ceb4b11032133d
Jonny Rimek <jimbo.db@protonmail.com>
2022-07-17 13:14:04 +0200

    Add networking and security groups

---
+++ tf/security-groups.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "alb" {
+  name   = "test-sg-alb"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name   = "test-sg-task"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = var.container_port
+    to_port          = var.container_port
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
--- tf/variables.tf
+++ tf/variables.tf
@@ -3,3 +3,8 @@ variable "app_count" {
   type = number
   default = 1
 }
+
+variable "container_port" {
+  type = number
+  default = 8080
+}
